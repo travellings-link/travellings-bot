f5675d726818fa6f39800f753805cb21f14a4656
Allenyou <i@allenyou.wang>
2024-11-13 20:27:13 +0800

    refactor: rewrite screenshotByID to reuse screenshotByUrl

--- src/methods/screenshot.ts
+++ src/methods/screenshot.ts
@@ -12,49 +12,14 @@ if (!fs.existsSync(tmpPath)) {
 	fs.mkdirSync(tmpPath);
 }
 
-function sleep(ms: number) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 async function screenshotByID(id: number) {
-	try {
 		const web = await WebModel.findByPk(id);
 
 		if (!web) {
 			throw new Error("没找到喵~ 你确定你输入的 ID 正确吗？");
 		}
 
-		const browser = await launch({
-			headless: true,
-			args: [
-				`--user-data-dir=${path.resolve(tmpPath)}`,
-				"--user-agent=Mozilla/5.0 (compatible; Travellings Check Bot; +https://www.travellings.cn/docs/qa)",
-				"--disable-logging",
-				"--log-level=3",
-				"--no-sandbox",
-				"--disable-setuid-sandbox",
-			],
-		});
-
-		const page = await browser.newPage();
-		await page.setViewport({ width: 1920, height: 1080 });
-		await page.setExtraHTTPHeaders({
-			referer: "https://www.travellings.cn/go.html", // 来自开往的 Referer
-		});
-		// await page.setDefaultNavigationTimeout(process.env.LOAD_TIMEOUT * 1000);
-
-		await page.goto(web.link);
-		await sleep(4500);
-		const screenshotBuffer = await page.screenshot();
-
-		await browser.close();
-		return screenshotBuffer;
-	} catch (e) {
-		if ((e as Error)["message"] !== undefined) {
-			logger.err((e as Error).message, "SCREENSHOT");
-		}
-		throw new Error("出错了喵~ 更多信息可能包含在控制台中~");
-	}
+	return await screenshotByUrl(web.link);
 }
 
 async function screenshotByUrl(url: string) {
