5104bae62d09b5c9cfb9660e4e750eea25164ce7
Allenyou <i@allenyou.wang>
2024-11-16 10:50:21 +0800

    feat: add debug log for DB

--- src/modules/sqlConfig.ts
+++ src/modules/sqlConfig.ts
@@ -8,12 +8,15 @@
 
 import { Sequelize } from "sequelize";
 import { config } from "../config";
+import { logger } from "./typedLogger";
 
 const sql = new Sequelize(config.DB_NAME, config.DB_USER, config.DB_PASSWORD, {
 	host: config.DB_HOST,
 	port: config.DB_PORT,
 	dialect: "mysql",
-	logging: false,
+	logging: (msg) => {
+		logger.debug(msg, "DB");
+	},
 });
 
 export default sql;
