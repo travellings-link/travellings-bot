1e6c35cc4818bee42bb62efb95921875478db295
gxres042 <i@gxres.net>
2025-01-07 22:39:10 +0800

    chore: delete alias in unbuild config

--- build.config.ts
+++ build.config.ts
@@ -2,8 +2,5 @@ import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
   entries: ["src/app.ts"],
-  outDir: "dist",
-  alias: {
-    "@bot": "./src/bot",
-  },
+  outDir: "dist"
 });
