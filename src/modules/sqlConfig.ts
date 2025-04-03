7e2481b6e1353d596216ec032c1d27f7ac382bdd
Allenyou <i@allenyou.wang>
2025-04-04 01:08:44 +0800

    fix: checkAll produces empty records in database (#54)

--- src/modules/sqlConfig.ts
+++ src/modules/sqlConfig.ts
@@ -5,10 +5,16 @@
 // |____/ \__\_\_____|     \____\___/|_| |_|_| |_|\__, |
 //                                                |___/
 // By BLxcwg666 <huixcwggmail.com>
+import cls from "cls-hooked";
 import { Sequelize } from "sequelize";
 
 import { config } from "../config";
 import { logger } from "./typedLogger";
+
+// 创建一个 CLS 命名空间，用于自动传递数据库事务
+const namespace = cls.createNamespace("travellings-bot-namespace");
+
+Sequelize.useCLS(namespace);
 
 const sql = new Sequelize(config.DB_NAME, config.DB_USER, config.DB_PASSWORD, {
 	host: config.DB_HOST,
--- src/utils/checkAll.ts
+++ src/utils/checkAll.ts
@@ -4,6 +4,7 @@ import { botManager } from "../bot/botManager";
 import { config } from "../config";
 import axiosCheck from "../methods/axios";
 import browserCheck from "../methods/browser";
+import sql from "../modules/sqlConfig";
 import { WebModel } from "../modules/sqlModel";
 import { logger } from "../modules/typedLogger";
 import { clearTravellingsAPICache } from "./clearTravellingsAPICache";
@@ -14,92 +15,95 @@ import { clearTravellingsAPICache } from "./clearTravellingsAPICache";
 export async function checkAll() {
 	logger.info("✓ 开始巡查站点", "APP");
 
-	// 备份巡查前的数据库
-	const webModels = await WebModel.findAll();
+	// 创建事务，并执行代码
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	await sql.transaction(async (_t) => {
+		await axiosCheck();
+		await browserCheck();
 
-	await axiosCheck();
-	await browserCheck();
-
-	const runWebsCount = await WebModel.count({
-		where: {
-			status: {
-				[Op.in]: ["RUN"],
+		const runWebsCount = await WebModel.count({
+			where: {
+				status: {
+					[Op.in]: ["RUN"],
+				},
+				lastManualCheck: {
+					[Op.or]: [
+						{ [Op.eq]: null },
+						{
+							[Op.lt]: new Date(
+								new Date().getTime() - 30 * 24 * 60 * 60 * 1000,
+							),
+						},
+					],
+				},
 			},
-			lastManualCheck: {
-				[Op.or]: [
-					{ [Op.eq]: null },
+		});
+		const allWebsCount = await WebModel.count({
+			where: {
+				status: {
+					[Op.notIn]: ["WAIT"],
+				},
+				lastManualCheck: {
+					[Op.or]: [
+						{ [Op.eq]: null },
+						{
+							[Op.lt]: new Date(
+								new Date().getTime() - 30 * 24 * 60 * 60 * 1000,
+							),
+						},
+					],
+				},
+			},
+		});
+
+		// 计算百分比：检查为 RUN 的站点数/需要检查的站点数
+		const runWebsPercentage = (runWebsCount / allWebsCount) * 100;
+		// 可接受的最低占比
+		const minRunSitesPercentage = config.MIN_RUN_SITES_PERCENTAGE;
+		if (runWebsPercentage < minRunSitesPercentage) {
+			logger.warn(
+				`△ 状态正常的站点占比低于 ${minRunSitesPercentage}%: ${runWebsPercentage.toFixed(2)}%`,
+				"APP",
+			);
+
+			// 调用开往 API 清除缓存
+			clearTravellingsAPICache(logger);
+
+			// 发送 bot 消息
+			botManager.boardcastRichTextMessage([
+				[{ type: "text", bold: true, content: "开往巡查姬提醒您：" }],
+				[{ type: "text", content: "" }],
+				[
 					{
-						[Op.lt]: new Date(
-							new Date().getTime() - 30 * 24 * 60 * 60 * 1000,
-						),
+						type: "text",
+						content: `巡查机巡查结果异常`,
 					},
 				],
-			},
-		},
-	});
-	const allWebsCount = await WebModel.count({
-		where: {
-			status: {
-				[Op.notIn]: ["WAIT"],
-			},
-			lastManualCheck: {
-				[Op.or]: [
-					{ [Op.eq]: null },
+				[
 					{
-						[Op.lt]: new Date(
-							new Date().getTime() - 30 * 24 * 60 * 60 * 1000,
-						),
+						type: "text",
+						bold: true,
+						content: `状态正常的站点占比为 ${runWebsPercentage.toFixed(2)}%`,
 					},
 				],
-			},
-		},
-	});
-	// 计算百分比：检查为 RUN 的站点数/需要检查的站点数
-	const runWebsPercentage = (runWebsCount / allWebsCount) * 100;
-	// 可接受的最低占比
-	const minRunSitesPercentage = config.MIN_RUN_SITES_PERCENTAGE;
-	if (runWebsPercentage < minRunSitesPercentage) {
-		logger.warn(
-			`△ 状态正常的站点占比低于 ${minRunSitesPercentage}%: ${runWebsPercentage.toFixed(2)}%`,
-			"APP",
-		);
-		// 撤回数据库修改
-		await WebModel.bulkCreate(webModels, { updateOnDuplicate: ["id"] });
+				[
+					{
+						type: "text",
+						content: `低于可接受的最低占比 ${minRunSitesPercentage}%`,
+					},
+				],
+				[{ type: "text", content: "" }],
+				[{ type: "text", content: `已自动撤回此次巡查的数据库修改` }],
+			]);
 
-		// 调用开往 API 清除缓存
-		clearTravellingsAPICache(logger);
-
-		// 发送 bot 消息
-		botManager.boardcastRichTextMessage([
-			[{ type: "text", bold: true, content: "开往巡查姬提醒您：" }],
-			[{ type: "text", content: "" }],
-			[
-				{
-					type: "text",
-					content: `巡查机巡查结果异常`,
-				},
-			],
-			[
-				{
-					type: "text",
-					bold: true,
-					content: `状态正常的站点占比为 ${runWebsPercentage.toFixed(2)}%`,
-				},
-			],
-			[
-				{
-					type: "text",
-					content: `低于可接受的最低占比 ${minRunSitesPercentage}%`,
-				},
-			],
-			[{ type: "text", content: "" }],
-			[{ type: "text", content: `已自动撤回此次巡查的数据库修改` }],
-		]);
-	} else {
+			// 抛出异常以撤回数据库修改
+			throw new Error("Rollback check results");
+		}
 		logger.ok(
 			`✓ 状态正常的站点占比 ${runWebsPercentage.toFixed(2)}%`,
 			"APP",
 		);
-	}
+	});
+
 	logger.ok("✓ 检测完成", "APP");
 }
